00748261f7da5a95bf40b18b241af4cb9065c65c
emilyniee <emily.nieu@gmail.com>
2024-06-17 21:29:56 -0400

    header to say work in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ import Projects from "./sections/projects";
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen min-w-screen">
+        <main className=" w-full flex justify-center fixed px-12 pb-1 pt-1 bg-coffee-100">
+            <h1 className="text-sm text-coffee-300">this website is a work in progress!</h1>
+        </main>
       <Navbar/>
       <Landing/>
       <About/>
