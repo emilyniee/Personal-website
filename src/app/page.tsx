7d69474591e902063026a6fa6519acf09141229e
emilyniee <emily.nieu@gmail.com>
2024-06-17 22:01:55 -0400

    linking to previous website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,13 @@ import Navbar from "./components/navbar";
 import About from "./sections/about";
 import Experience from "./sections/experience";
 import Projects from "./sections/projects";
+import Wip from "./sections/wip";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen min-w-screen">
-        <main className=" w-full flex justify-center fixed px-12 pb-1 pt-1 bg-coffee-100">
-            <h1 className="text-sm text-coffee-300">this website is a work in progress!</h1>
-        </main>
       <Navbar/>
+      <Wip/>
       <Landing/>
       <About/>
       <Experience/>
---
+++ src/app/sections/wip.tsx
@@ -0,0 +1,16 @@
+export default function Projects() {
+    return (
+    <main className="w-full flex justify-center fixed px-12 pb-1 pt-1 bg-coffee-100">
+  <h1 className="text-sm text-coffee-300">
+    this website is a work in progress! See previous version 
+    <a 
+      href="https://emily-nie-v2.vercel.app" 
+      target="_blank" 
+      className="text-sm text-coffee-300 underline hover:text-coffee-400 ml-1"
+    >
+      here
+    </a>
+  </h1>
+  
+</main>
+    )}
