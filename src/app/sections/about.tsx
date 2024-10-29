4d14f182fbe43668898723ad657e80f23a1cec9c
emilyniee <emily.nieu@gmail.com>
2024-10-29 02:07:54 -0400

    adding information to about me

--- src/app/sections/about.tsx
+++ src/app/sections/about.tsx
@@ -17,8 +17,34 @@ export default function About() {
             />
             </div>
         </div>
-        <div className="m-4 bg-coffee-200 w-3/4 h-4/6 p-4 rounded-md border border-coffee-300">
-                
+        <div className="m-4 bg-coffee-200 w-3/4 h-4/6 p-4 rounded-md border border-coffee-300 text-black">
+            <div className="mb-4">
+              Hi! My name is Emily Nie and I’m a 3rd year Computer Science student at the University of Waterloo. I’m a problem solver who thrives on innovation and challenges.
+            </div>
+            <div className="mb-4">
+              <p className="font-semibold">Tech stack:</p>
+              <ul className="ml-4">
+                <li>○ Languages: Python, Javascript, Typescript, Java, C/C++, C#, Go, Bash, Groovy, HTML, CSS/SASS, SQL</li>
+                <li>○ Libraries/Frameworks: React, Next.js, Vue.js, Angular, Django, Node.js, Express.js, Bootstrap, PyTorch, TensorFlow</li>
+                <li>○ Tools: PostgreSQL, MySQL, DynamoDB, MongoDB, Terraform, Docker, Git, Jenkins, AWS, Azure, Figma, Jira</li>
+              </ul>
+            </div>
+            <div className="mb-4">
+              <p className="font-semibold">Experience: </p>
+              <ul className="ml-4">
+                <li>○ Current SWE intern @ AMD, Full-stack Developer @ UW Blueprint</li>
+                <li>○ Previous SWE @ Trend Micro, Full-stack Developer @ RCAF </li>
+              </ul>
+            </div>
+            <div className="mb-4">
+              <p className="font-semibold">Hobbies: </p>
+              <ul className="ml-4">
+                <li>○ Dance! I’m the director of a dance crew called Haebeat. In my spare time, you’ll probably catch me at a dance studio</li>
+                <li>○ Drawing, all of the icons and visuals on this website are designed or drawn by me :) </li>
+                <li>○ Film - I like to preserve memories in pictures and videos</li>
+                <li>○ Gaming (Valorant, TFT, Word Hunt, to name some) </li>
+              </ul>
+            </div>
         </div>
     </main>
   )
